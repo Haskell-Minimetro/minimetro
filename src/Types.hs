df3861f8c6257d28be7582066c52efe136014067
Vyacheslav Goreev <v.goreev@innopolis.ru>
2020-10-02 12:29:09 +0300

    feat: Add basic data types

---
+++ src/Types.hs
@@ -0,0 +1,14 @@
+data Asset = Bridge | Train | Wagon
+data Object = Object Asset Position
+type ObjectDrawer = Object -> Picture
+
+
+data StationType = Triangle | Rectangle | Circle
+data Passenger = Passenger StationType
+data Station = Station StationType Position [Passenger]
+
+
+data Route = Route Color 
+
+
+data GameState = GameState 
